f47189e3dcd8ec0f053b967c20679da367701f31
Daniel Kovac <danijelkovacs2@gmail.com>
2018-01-29 09:58:30 +0100

    Add files via upload

---
+++ scrollspy.js
@@ -0,0 +1,19 @@
+
+    /*Scroll Spy*/
+    $('body').scrollspy({ target: '.navbar', offset:80});
+
+    /*Smooth link animation*/
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1200);
+				
+                
+            }
+        }
+    });
